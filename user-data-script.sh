dfb78dea2bbd04001253d286f447d68401199cd7
Wojciech Kaczmarczyk <wojciech.kaczmarczyk@god-se.com>
2021-03-20 10:38:55 +0100

    fix issues + install sonarqube server

--- user-data-script.sh
+++ user-data-script.sh
@@ -2,5 +2,8 @@
 USER='ec2-user'
 yum update -y
 yum install -y docker git vim nodejs java-11-openjdk-devel
-sudo usermod -aG docker $USER 
+usermod -aG docker $USER 
 service docker enable
+service docker start
+# Install sonarqube server 
+docker run -d --name sonarqube -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true -p 9000:9000 sonarqube:latest
